1364ebde9e6ccc9923b0d63ed0b43d8a6fb8b278
Jeremy Kwa <jkwa@ucsd.edu>
2017-11-03 14:47:27 -0700

    Before-IPM-Nov-3

--- src/Shop.js
+++ src/Shop.js
@@ -1,84 +1,38 @@
+const AgedBrieItemUpdateRates = require('./UpdateRates/AgedBrieItemUpdateRates');
+const ConjuredItemUpdateRates = require('./UpdateRates/ConjuredItemUpdateRates');
+const SulfurasItemUpdateRates = require('./UpdateRates/SulfurasItemUpdateRates');
+const BackstagePassesItemUpdateRates = require('./UpdateRates/BackstagePassesItemUpdateRates');
+const ItemUpdateRatesConstructor = require('./UpdateRates/ItemUpdateRates');
+const ItemUpdateRates = new ItemUpdateRatesConstructor();
+
 class Shop {
-  constructor(items=[]){
-    this.items = items;
-  }
+ constructor(items=[]){
+   this.items = items;
+ }
 
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      var item = this.items[i];
-      var name = item.name;
-      if (name == 'Sulfuras, Hand of Ragnaros') {
-        continue;
-      }
-      else if (name == 'Aged Brie') {
-        updateBrie(item);
-      }
-      else if (name == 'Backstage passes to a TAFKAL80ETC concert') {
-        updateConcertTicket(item);
-      } 
-      else if (name == 'Conjured') {
-        updateConjuredItem(item);
-      }
-      else {
-        updateRegularItem(item);
-      }
-      updateSellIn(item);
-      checkQualityBounds(item);
-    }
-    return this.items;
-
-    function updateSellIn(item) {
-      item.sellIn--;
-    }
-
-    function updateBrie(item) {
-      if (item.quality < 50) {
-        item.quality++;
-      }
-    }
-
-    function updateConcertTicket(item) {
-      if (item.sellIn <= 0) {
-        item.quality = 0;
-      }
-      else if (item.sellIn < 6) {
-        item.quality += 3
-      }
-      else if (item.sellIn < 11) {
-        item.quality += 2;
-      }
-      else {
-        item.quality++;
-      }
-    }
-    
-    function updateConjuredItem(item) {
-      if (item.sellIn <= 0) {
-        item.quality -= 4;
-      }
-      else {
-        item.quality -= 2;
-      }
-    }
-
-    function updateRegularItem(item) {
-      if (item.sellIn <= 0) {
-        item.quality -= 2;
-      }
-      else {
-        item.quality--;
-      }
-    }
-
-    function checkQualityBounds(item) {
-      if (item.quality > 50) {
-        item.quality = 50;
-      }
-      if (item.quality < 0) {
-        item.quality = 0;
-      }
-    }
-  }
+ updateQuality() {
+   for (var i = 0; i < this.items.length; i++) {
+     var item = this.items[i];
+     var name = item.name;
+     if (name === 'Sulfuras, Hand of Ragnaros') {
+       SulfurasItemUpdateRates.updateItem(item);
+     }
+     else if (name === 'Aged Brie') {
+       AgedBrieItemUpdateRates.updateItem(item);
+     }
+     else if (name === 'Backstage passes to a TAFKAL80ETC concert') {
+       BackstagePassesItemUpdateRates.updateItem(item);
+     }
+     else if (name === 'Conjured') {
+       ConjuredItemUpdateRates.updateItem(item);
+     }
+     else {
+       ItemUpdateRates.updateItem(item);
+     }
+     
+   }
+   return this.items;
+ }
 }
 
 module.exports = Shop;
